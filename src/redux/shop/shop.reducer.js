e9c5009e5eb39d39ac82170ffad8f8d7f15154d0
Muhammad Irfan <irfanmuh75@gmail.com>
2021-07-02 15:07:04 +0700

    updating code to leverage containers for collections-overview and collection-page component

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,14 +2,28 @@ import ShopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
   collections: null,
+  isLoading: false,
+  errorMessage: undefined,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopActionTypes.UPDATE_COLLECTION:
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
       return {
         ...state,
+        isLoading: true,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
         collections: action.payload,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: action.payload,
       };
     default:
       return state;
